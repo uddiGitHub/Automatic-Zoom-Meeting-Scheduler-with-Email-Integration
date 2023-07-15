47319ef9c1b546e51dd2073a22995ba0e0d3e9cb
uddiGitHub <u20cse1071@cit.ac.in>
2023-07-15 16:22:12 +0530

    v0.5

--- src/credentials.py
+++ src/credentials.py
@@ -1,5 +1,5 @@
 # this file stores the credentials for login purpose
 emailId = 'iocl.internship23@gmail.com'
-gmail_password = 'internship@iocl23'
-zoom_password = 'internship@IOCL23'
+gmail_password = '[your gamil password]'
+zoom_password = '[your zoom password]'
 
