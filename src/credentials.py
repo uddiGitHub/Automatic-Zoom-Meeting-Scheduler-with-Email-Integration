4f8d46c7fcae80824de98a3d68aeacef9f519576
uddiGitHub <u20cse1071@cit.ac.in>
2023-07-24 14:34:50 +0530

    v0.5

--- src/credentials.py
+++ src/credentials.py
@@ -1,6 +1,6 @@
 # this file stores the credentials for login purpose
 emailId = 'iocl.internship23@gmail.com'
-gmail_password = 'internship@iocl23'
-zoom_password = 'internship@IOCL23'
+gmail_password = '[your gamil password]'
+zoom_password = '[your zoom password]'
 
-bard_api_key = 'YQjk8uk5gDyRA0A_moBOGTBhy_nYc68LkPRlGAaFNbI8bxyEgXbi9jgGNmh_0t4KmMn4jA.'
+bard_api_key = '[Bard API key]'
